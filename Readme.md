4ef449b2b6f3f4f6cb5f456f72b11c171caafbfd
Gurriato <williamwhite.blanco@gmail.com>
2023-11-06 02:58:57 +0100

    improvement over main function and code cleaning

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 Still WIP but working
 
 Why I built this scrapper? because others had a lot of inconsistencies. Even with errors this is most accurate Oryx scrapper you are going to find.
-Why others are not as accurate? because Oryx blog is manually feed. Therefore, there are a lot of exceptions you had to code in order to capture all the data.
+Why others are not as accurate? because Oryx blog is manually feed. Therefore, there are a lot of exceptions you had to code in order to capture all the data and all other projects were created long time ago, so there are new exceptions not capture by their code.
 
 Commits are welcome
 
@@ -10,7 +10,7 @@ Commits are welcome
 There are some equipment not been capture by the scrapper due coding errors on Oryx webpage
 - Coding errors in Oryx page
     - 2 Ucranian tanks (2 M-55S)
-        - Coding error in Oryx webpage
+        - Coding error in Oryx webpage, my code is unable to capture this, added manually
     - 29 unkown Russian Equipment. Impact on the df: (probably most of them are because they are )
         - 11 220mm TOS-1A appearing as MT-LB with 140mm Ogon-18 MRLS
         - UAZ PAtriot jeep labeled as LUAZ-969
@@ -18,25 +18,50 @@ There are some equipment not been capture by the scrapper due coding errors on O
             - Still missing 10 elements 😓
 - Data errors in Oryx page:
     - UA:
-        - 1 UAZ-3151: (23, destroyed) -> should be  1 UAZ-3151: (1, destroyed) On Russian equipment
-        - BMP-1(P) proof 64 destroyed is missingm therefore, total number 348 (+1) (@31/10/2023)
+        - 1 UAZ-3151: (23, destroyed) -> should be  1 UAZ-3151: (1, destroyed) On Russian equipment (@31/10/2023) Fixed on an update(@06/11/2023)
+        - BMP-1(P) proof 64 destroyed is missing, also miscounted, total number 349 (+1) (@31/10/2023)
         - BMP-2(K) is miscounted. Total number == 181 (-1) (@31/10/2023)
         - Unknown BMP-1/2 is miscounted. Total number == 27 (-1) (@31/10/2023)
         - BTR-70 proof 1 is missing, therefore total number = 28 (-1) (@31/10/2023)
         - AT105A Saxon is miscounted, total number = 13 (-1) (@31/10/2023)
         - 155mm M777A2 howitzer is miscounted, total number = 70 (+1) (@31/10/2023)
-        - 122mm BM-21 Grad is missing proof 16, teherfore total number 37 (-1)
+        - 122mm BM-21 Grad is missing proof 16, therefore total number 37 (-1) (@31/10/2023)
         - 5P85D/S (launcher for S-300PS) 13 and 14 are repeated? (@31/10/2023)
         - A1-SM Fury case 35 = 32 and miscounted. Total number = 55 (-1) (@31/10/2023)
         - KrAZ-255B miscounted total number 16(-1) (@31/10/2023)
         - GAZ-66 Format on first case, also miscounted total 107(-1) (@31/10/2023)
         - MAZ-537 miscounted, total 3 (+1) (@31/10/2023)
-        - VEPR is duplicated, one under Armoured Fighting Vehicles the other  under Mine-Resistant Ambush Protected (MRAP)
+        - VEPR is duplicated, one under Armoured Fighting Vehicles the other under Mine-Resistant Ambush Protected (MRAP)
+        - ZiL-131 case 71 format (@06/11/2023)
     - RU:
-        - Pending...
+        - T-72B3 miscounted, total 347 (-2)
+        - T-72B3 Obr. 2016 miscounted, total 240 (-1)
+        - T-80BV miscounted, total 422 (-1)
+        - T-80U miscounted, total 93 (-1)
+        - unkown tank, miscounted, total 287 (-3)
+        - MT-LB miscounted, total 508 (+1)
+        - Unknown AFV miscounted, total 208 (+1)
+        - BMP-1(P) miscounted, total 543 (-2)
+        - BMP-3 miscounted, total 296 (-1)
+        - PTS-2  tracked amphibious transport, miscounted, total 15 (-1)
+        - PMP floating bridge miscounted, total 11 (-1)
+        - 9P149 Shturm-S miscounted, total 36 (-1)
+        - 100mm MT-12 anti-tank gun miscounted, total 28 (-1)
+        - 152mm2S5 Giatsint-S, total 52 (+1)
+        - 152mm 2S19 Msta-S, total 147 (-5)
+        - 152mm 2S33 Msta-SM2, total 34 (-1)
+        - 122mm BM-21 Grad, total 177 (+1)
 
 - Errors in my code:
     - 3388-3400 UA rows missing name they are 13 9A310M1 TELAR. Total number of cases is correct.
+    - 1V110 BM-21 Grad battery command vehicle, bad scrap
+    - 2V15M fire control and observation vehicle, bad scrap
+    - 1V16battalion fire direction vehicle, bad scrap
+    - 19 122mm 2B17 Tornado-G labeled as 122mm 2B26 Grad-K
+    - 220mm TOS-1A labeled as MT-LB with 140mm Ogon-18 MRLS
+    - 14 9A310M1-2 TELAR (for Buk-M1-2) missing names around rows ~9017-9120
+    - more un RU side
+
 
 
 
@@ -63,3 +88,4 @@ There are some equipment not been capture by the scrapper due coding errors on O
 ## Other related projects to explore
 - https://github.com/leedrake5/Russia-Ukraine
 - https://github.com/scarnecchia/scrape_oryx
+
--- main.py
+++ main.py
@@ -19,71 +19,100 @@ import matplotlib
 
 
 import configparser
+def scrappingOryx():
+    import hashlib
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+    def generate_short_hash(row):
+        hash_input = '_'.join([str(row['weapon']), str(row['platform']), str(row['number']), str(row['url'])])
+        return hashlib.sha256(hash_input.encode()).hexdigest()[:20]  #
 
-column_order = ['owner', 'origen', 'weapon', 'platform', 'total', 'number', 'Status', 'url']
+    column_order = ['owner', 'origen', 'weapon', 'platform', 'total', 'number', 'Status', 'url']
 
-# Russia scrap
-urlRU = 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html'
-scraped_object = tools.extract_lists_from_article(urlRU)
-RU, total_RU = tools.trans_to_df(scraped_object)
-RU['origen'] = RU['origen'].apply(tools.extr_country)
-RU[['total', 'platform']] = RU['platform'].str.extract(r'(\d+)\s?([^:]+)', expand=True)
-RU['owner'] = 'RU'
-RU = RU[column_order]
+    # Russia scrap
+    urlRU = 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html'
+    scraped_object = tools.extract_lists_from_article(urlRU)
+    RU, total_RU = tools.trans_to_df(scraped_object)
+    RU['origen'] = RU['origen'].apply(tools.extr_country)
+    RU[['total', 'platform']] = RU['platform'].str.extract(r'(\d+)\s?([^:]+)', expand=True)
+    RU['owner'] = 'RU'
+    RU = RU[column_order]
 
-# Ucrania scrap
-urlUA = 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html'
-scraped_object = tools.extract_lists_from_article(urlUA)
-UA, total_UA = tools.trans_to_df(scraped_object)
-UA['origen'] = UA['origen'].apply(tools.extr_country)
-UA[['total', 'platform']] = UA['platform'].str.extract(r'(\d+)\s?([^:]+)', expand=True)
-UA['owner'] = 'UA'
-UA = UA[column_order]
+    # Ucrania scrap
+    urlUA = 'https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html'
+    scraped_object = tools.extract_lists_from_article(urlUA)
+    UA, total_UA = tools.trans_to_df(scraped_object)
+    UA['origen'] = UA['origen'].apply(tools.extr_country)
+    UA[['total', 'platform']] = UA['platform'].str.extract(r'(\d+)\s?([^:]+)', expand=True)
+    UA['owner'] = 'UA'
+    UA = UA[column_order]
 
-# concat and final cleaning
-to_file = pd.concat([RU, UA])
-to_file = to_file.reset_index(drop=True)
-to_file['url'] = to_file['url'].apply(tools.clean_url)
+    # concat and final cleaning
+    to_file = pd.concat([RU, UA])
+    to_file = to_file.reset_index(drop=True)
+    to_file['url'] = to_file['url'].apply(tools.clean_url)
 
-# Memory
-scraped_object, UA, RU = None, None, None
-gc.collect()
+    # Memory
+    scraped_object, UA, RU = None, None, None
+    gc.collect()
 
+    to_file['primary_key'] = to_file.apply(generate_short_hash, axis=1)
 
-to_report = to_file.copy()
-to_report['Status'] = to_report['Status'].astype(str)
-to_report['origen'] = to_report['origen'].astype(str)
-outcome = tools.insertar_en_bbdd(to_report, config['usuario'], config['pass'], config['host'], config['database'], 'bajas')
-now = dt.datetime.now()
-to_file.to_csv('./Data/scrap_raw_' + str(now.date()) + '.csv')
+    to_report = to_file.copy()
+    to_report['Status'] = to_report['Status'].astype(str)
+    to_report['origen'] = to_report['origen'].astype(str)
+    outcome = tools.insertar_en_bbdd(to_report, config['usuario'], config['pass'], config['host'], config['database'], 'bajas')
+    print(outcome)
+    now = dt.datetime.now()
+    to_file.to_csv('./Data/scrap_raw_' + str(now.date()) + '.csv')
 
-# Transforming Totals
-regex = r"^(.+?)\s*\((\d+), of which( destroyed: (\d+))?(, damaged: (\d+))?(, abandoned: (\d+))?(, captured: (\d+))?\)$"
-total_RU = pd.Series(total_RU)
-total_RU = total_RU.str.extract(regex)
-total_RU.columns = ['Type', 'Total', 'd1', 'Destroyed', 'd2', 'Damaged', 'd3', 'Abandoned', 'd4', 'Captured']
-total_RU.drop(['d1', 'd2', 'd3', 'd4'], axis=1, inplace=True)
-total_RU = total_RU[1:]
-total_RU.fillna(0, inplace=True)
-total_RU[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']] = total_RU[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']].astype(int)
-outcome = tools.insertar_en_bbdd(total_RU, config['usuario'], config['pass'], config['host'], config['database'], 'totals_RU')
-total_RU.to_csv('./Data/scrap_Total_RU' + str(now.date()) + '.csv')
+    # Transforming Totals
+    regex = r"^(.+?)\s*\((\d+), of which( destroyed: (\d+))?(, damaged: (\d+))?(, abandoned: (\d+))?(, captured: (\d+))?\)$"
+    total_RU = pd.Series(total_RU)
+    total_RU = total_RU.str.extract(regex)
+    total_RU.columns = ['Type', 'Total', 'd1', 'Destroyed', 'd2', 'Damaged', 'd3', 'Abandoned', 'd4', 'Captured']
+    total_RU.drop(['d1', 'd2', 'd3', 'd4'], axis=1, inplace=True)
+    total_RU = total_RU[1:]
+    total_RU.fillna(0, inplace=True)
+    total_RU[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']] = total_RU[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']].astype(int)
+    outcome = tools.insertar_en_bbdd(total_RU, config['usuario'], config['pass'], config['host'], config['database'], 'totals_RU')
+    total_RU.to_csv('./Data/scrap_Total_RU' + str(now.date()) + '.csv')
 
-total_UA = pd.Series(total_UA)
-total_UA = total_UA.str.extract(regex)
-total_UA.columns = ['Type', 'Total', 'd1', 'Destroyed', 'd2', 'Damaged', 'd3', 'Abandoned', 'd4', 'Captured']
-total_UA.drop(['d1', 'd2', 'd3', 'd4'], axis=1, inplace=True)
-total_UA = total_UA[1:]
-total_UA.fillna(0, inplace=True)
-total_UA[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']] = total_UA[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']].astype(int)
-outcome = tools.insertar_en_bbdd(total_UA, config['usuario'], config['pass'], config['host'], config['database'], 'totals_UA')
-total_UA.to_csv('./Data/scrap_Total_UA_' + str(now.date()) + '.csv')
+    total_UA = pd.Series(total_UA)
+    total_UA = total_UA.str.extract(regex)
+    total_UA.columns = ['Type', 'Total', 'd1', 'Destroyed', 'd2', 'Damaged', 'd3', 'Abandoned', 'd4', 'Captured']
+    total_UA.drop(['d1', 'd2', 'd3', 'd4'], axis=1, inplace=True)
+    total_UA = total_UA[1:]
+    total_UA.fillna(0, inplace=True)
+    total_UA[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']] = total_UA[['Total', 'Destroyed', 'Damaged', 'Abandoned', 'Captured']].astype(int)
+    outcome = tools.insertar_en_bbdd(total_UA, config['usuario'], config['pass'], config['host'], config['database'], 'totals_UA')
+    total_UA.to_csv('./Data/scrap_Total_UA_' + str(now.date()) + '.csv')
+    return to_file, total_UA, total_RU
+def dataReport():
+    from ydata_profiling import ProfileReport
+    report = pd.read_csv(r'.\Data\scrap_raw.csv')
+    profile = ProfileReport(report, title="Oryx Data Report")
+    profile.to_file(r'.\informe.html')
 
-#from ydata_profiling import ProfileReport
-#report = pd.read_csv(r'C:\Users\gblancom\PycharmProjects\pythonProject\scrap_raw.csv')
-#profile = ProfileReport(report, title="Informe de perfil de pandas")
-#profile.to_file(r'C:\Users\gblancom\PycharmProjects\pythonProject\informe.html')
+def comparar_dataframes(df_a, df_b, df_excepciones):
+    # Convertir las primary keys de excepciones en un set para rápida referencia
+    excepciones_a = set(df_excepciones['primary_key_a'])
+    excepciones_b = set(df_excepciones['primary_key_b'])
 
+    # Filtrar df_b para excluir las filas que tienen primary keys en el set de excepciones
+    df_b_filtrado = df_b[~df_b['primary_key'].isin(excepciones_b)]
+
+    # Identificar las filas en df_b que no están en df_a
+    df_a_keys = set(df_a['primary_key'])
+    df_b_filtrado = df_b_filtrado[~df_b_filtrado['primary_key'].isin(df_a_keys)]
+
+    return df_b_filtrado
+
+# Aplicar la función a los DataFrames de ejemplo
+
+if __name__ == '__main__':
+    to_file, total_UA, total_RU = scrappingOryx()
+    exceptions = pd.read_csv('./Data/exceptions.csv', sep=';')
+    base = pd.read_csv('./Data/base.csv', sep=';')
+    df_resultado = comparar_dataframes(base, to_file, exceptions) #nuevas lineas a incluir
